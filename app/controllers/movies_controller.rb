e52eecf73dd5828e77ab90c1aefa4358532ad30e
Ryan Sonnek <ryan.sonnek@gmail.com>
2012-10-10 15:36:57 -0500

    add in progress behavior

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,7 @@ class MoviesController < ApplicationController
   end
 
   def search
+    sleep 1 # simulate latency
     @movies = Movie.where('title like ?', "#{params[:q]}%").order("title asc").limit(10)
     @results = @movies.collect do |m|
       {:label => m.title}
