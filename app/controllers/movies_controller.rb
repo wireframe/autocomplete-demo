730ccf939979271fc4a47b0b290e273472aff521
Ryan Sonnek <ryan.sonnek@gmail.com>
2012-10-10 16:31:06 -0500

    extract before_filter

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,20 @@
 class MoviesController < ApplicationController
+  before_filter :simulate_latency, :only => :search
   def index
   end
 
   def search
-    sleep 1 # simulate latency
     @movies = Movie.where('title like ?', "#{params[:q]}%").order("title asc").limit(10)
     @results = @movies.collect do |m|
       {:label => m.title}
     end
     render :json => @results
+  end
+
+  private
+  # simple hack to accentuate local search results
+  # if the server is too fast, local search results aren't really that useful
+  def simulate_latency
+    sleep 0.5
   end
 end
